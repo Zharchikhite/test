da360af083e4cffeebe38a3089567baf84f5ab96
Zharchikhite <31092387+Zharchikhite@users.noreply.github.com>
2017-08-17 20:27:20 +0200

    a

--- mhm/js/hi.js
+++ mhm/js/hi.js
@@ -1,25 +1,11 @@
-/*function openNav(){
-  document.getElementById("nav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  console.log("open");
-}
-
-function closeNav(){
-  document.getElementById("nav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
-  console.log("close");
-}*/
-
 function openNav(){
-  if(document.getElementById("menu").className == "test"){
+  if(document.getElementById("menu").className == "closed"){
     document.getElementById("nav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("menu").className = "test1";
-    console.log("1");
+    document.getElementById("menu").className = "open";
   }else{
     document.getElementById("nav").style.width = "0px";
     document.getElementById("main").style.marginLeft = "0px";
-    document.getElementById("menu").className = "test";
-    console.log("2");
+    document.getElementById("menu").className = "closed";
   }
 }
